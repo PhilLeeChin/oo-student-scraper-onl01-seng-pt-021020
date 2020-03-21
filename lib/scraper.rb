74691cfbcedcee30813c8deed5872c88cd682fda
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-21 19:20:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,18 +7,15 @@ class Scraper
   def self.scrape_index_page(index_url)
     student = []
     html = Nokogiri::HTML(open(index_url))
-    index = profile_page.css("div.social-icon-container").children.css("a").map (|el| el.attributes('herf').value)
-    index.each do |link|
-      if link.include?("linkedin")
-        student[:location] = link
-      elsif link.include?("github")
-        student[:github] = link
-      elsif link.include?("twitter")
-        student[:twitter] = link
-      else
-        student[:blog] = link
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
     end
+    student
   end
 
   def self.scrape_profile_page(profile_url)
